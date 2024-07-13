4738e3d427c2b0a7040899cbb8bf4d6016b280cd
Alberto Harka <alb.harka@icloud.com>
2024-07-13 18:06:03 +0200

    Added docker image creation for beacon and relay

--- relay/run.sh
+++
@@ -1,9 +0,0 @@
-PORT=44443 HOST=localhost PUSH_KEY=debug tsx src/index.ts localhost:3001 &
-P1=$!
-PORT=44444 HOST=localhost PUSH_KEY=debug tsx src/index.ts localhost:3001 &
-P2=$!
-PORT=44445 HOST=localhost PUSH_KEY=debug tsx src/index.ts localhost:3001 &
-P3=$!
-PORT=44446 HOST=localhost PUSH_KEY=debug tsx src/index.ts localhost:3001 &
-P4=$!
-wait $P1 $P2 $P3 $P4
