9c2446d9e30b577e06fa76b3fa97e32ac74c77a2
Alberto Harka <alb.harka@icloud.com>
2024-07-15 00:06:14 +0200

    fixed references to common package

--- packages/nexus/test/integration.test.ts
+++ packages/nexus/test/integration.test.ts
@@ -1,9 +1,9 @@
 import { describe, it, before, after } from "node:test";
 import assert from "node:assert";
-import { exec as execAsync, sleep, useTimeout } from "common/src/utils";
+import { exec as execAsync, sleep, useTimeout } from "common/utils";
 import { exec } from "node:child_process";
 
-import { NexusTestClient, nexusWebsocket } from "common/src/utils/clients";
+import { NexusTestClient, nexusWebsocket } from "common/utils/clients";
 import {
   TEST_APP_KEY,
   TEST_HOST,
